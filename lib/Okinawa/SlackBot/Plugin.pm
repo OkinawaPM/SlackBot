419df3e488564073d26a2b69370330edfbe14cf3
K <x00.x7f@gmail.com>
2016-09-07 14:58:09 +0900

    load 時 new, load, DESTROY メソッドを削除する機能の追加

--- lib/Okinawa/SlackBot/Plugin.pm
+++ lib/Okinawa/SlackBot/Plugin.pm
@@ -1,36 +1,40 @@
 package Okinawa::SlackBot::Plugin;
 
 use Okinawa::Base -base;
+use Data::Dumper;
 extends 'Okinawa::SlackBot';
 
 sub load {
-	my $self = shift;
-	my $path = classpath(__PACKAGE__);
+    my $self = shift;
+    my $path = classpath(__PACKAGE__);
 
-	# package list under "Plugin" directory
-	opendir(DH, $path) or die "Could not opendir: $!";
-	my @pm = grep { $_ !~ /^\.+$/ } readdir(DH);
-	closedir(DH);
+    # package list under "Plugin" directory
+    opendir(DH, $path) or die "Could not opendir: $!";
+    my @pm = grep { $_ !~ /^\.+$/ } readdir(DH);
+    closedir(DH);
 
-	# load plugins
-	eval {
-		for my $plugin (@pm) {
-			require "$path/$plugin";
-			$plugin =~ s/\.pm$//;
-			my $method = lc $plugin;
-			my $package = __PACKAGE__."::$plugin";
-			$self->log->info("Load method: $package->$method");
+    # load plugins
+    eval {
+        for my $plugin (@pm) {
+            require "$path/$plugin";
+            $plugin =~ s/\.pm$//;
+            my $method = lc $plugin;
+            my $package = __PACKAGE__."::$plugin";
+            $self->log->info("Load method: $package->$method");
 
-			# About "sub { $package->$method(pop @_) }"
-			# @_ == ([0] => $class, [1] => $argument)
-			# So remove first element "Okinawa::SlackBot::Plugin" class
-			$self->meta->add_method($method => sub { $package->$method(pop @_) });
-		}
-	};
-	if ($@) {
-		$self->log->warn($@);
-	}
-	return $self;
+            # About "sub { $package->$method(pop @_) }"
+            # @_ == ([0] => $class, [1] => $argument)
+            # So remove first element "Okinawa::SlackBot::Plugin" class
+            $self->meta->add_method($method => sub { $package->$method(pop @_) });
+        }
+    };
+
+    $self->log->warn($@) if $@;
+
+    # Remove main class
+    delete $self->meta->{methods}->{$_} for qw/new meta DESTROY/;
+
+    return $self;
 }
 
 __PACKAGE__->meta->make_immutable();
